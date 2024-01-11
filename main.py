ddda26d49e87c3ee20a9210d47cff6c2c87d7e34
Никита Колесник <nikitakolesnik@MacBook-Pro-Nikita.local>
2024-01-11 18:31:22 +0400

    ggggg

--- main.py
+++ main.py
@@ -114,7 +114,7 @@ async def run_parser(city, search_query):
 
 async def main():
     city = "samara"
-    search_query = "Вкусно и точка"
+    search_query = "Магазин телефонов"
     await run_parser(city, search_query)
 
 
