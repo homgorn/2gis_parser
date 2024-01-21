af18084db2f49e78e85ab71ea8bea305a023f8e0
Никита Колесник <nikitakolesnik@MacBook-Pro-Nikita.local>
2024-01-21 18:28:07 +0400

    f

--- main.py
+++ main.py
@@ -135,14 +135,6 @@ async def run_parser(city, search_query):
                         await find_and_get_elements(driver, main_block, data_in_memory)
                     except TelegramNetworkError:
                         continue
-            except InvalidSessionIdException as e:
-                print(f"Произошло исключение InvalidSessionIdException: {e}")
-                current_page = driver.current_url
-                driver.close()
-                driver.quit()
-                time.sleep(10)
-                driver.get(current_page)
-                continue
             except TelegramNetworkError:
                 continue
 
@@ -165,7 +157,7 @@ async def run_parser(city, search_query):
         current_page = driver.current_url
         driver.close()
         driver.quit()
-        time.sleep(10)
+        time.sleep(20)
         driver.get(current_page)
         pass
 
